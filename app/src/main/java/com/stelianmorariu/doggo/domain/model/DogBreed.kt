31c18f04b926ab9ebee3d1c087b214c48ab65699
Stelian Morariu <stelian.morariu@gmail.com>
2020-06-22 16:57:01 +0100

    add domain model

---
+++ app/src/main/java/com/stelianmorariu/doggo/domain/model/DogBreed.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Stelian Morariu 2020.
+ */
+
+package com.stelianmorariu.doggo.domain.model
+
+/**
+ * Our domain model represents the source of truth for the  presentation layer.
+ * Data acquired from different sources will be mapped to this model.
+ */
+data class DogBreed(
+    val displayName: String,
+    val breed: String,
+    val subBreed: String = ""
+)
