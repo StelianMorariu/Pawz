c1525ba5726d7673a2b44ef0935957dd6c5b6cf2
Stelian Morariu <stelian.morariu@gmail.com>
2020-06-22 19:50:48 +0100

    let Dagger init and provide the retrofit service

--- app/src/main/java/com/stelianmorariu/doggo/domain/dagger/modules/DoggoAppModule.kt
+++ app/src/main/java/com/stelianmorariu/doggo/domain/dagger/modules/DoggoAppModule.kt
@@ -7,9 +7,14 @@ package com.stelianmorariu.doggo.domain.dagger.modules
 import android.app.Application
 import android.content.Context
 import com.stelianmorariu.doggo.BuildConfig
+import com.stelianmorariu.doggo.data.network.DogApiService
 import com.stelianmorariu.doggo.domain.DoggoConfig
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 /**
@@ -36,4 +41,37 @@ class DoggoAppModule {
         DoggoConfig(BuildConfig.LOGS_ENABLED, BuildConfig.DOG_API_URL)
 
 
+    @Provides
+    @Singleton
+    fun provideDogApi(retrofit: Retrofit) = retrofit.create(DogApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(config: DoggoConfig, client: OkHttpClient) =
+        Retrofit.Builder()
+            .baseUrl(config.getSafeUrl())
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+//            .addCallAdapterFactory(RxErrorHandlingCallAdapterFactory.create())
+            .build()
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofitClient(loggingInterceptor: HttpLoggingInterceptor) =
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            // add timeouts or certificate pinner
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofitLoggingInterceptor(config: DoggoConfig) =
+        HttpLoggingInterceptor().apply {
+            level = if (config.enableLogs) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
 }
