63a0cebbf5a70dd26ef0fe8bca54a7842979f604
Stelian Morariu <stelian.morariu@gmail.com>
2020-06-23 13:29:59 +0100

    navigate to breeds list activity

--- app/src/main/java/com/stelianmorariu/pawz/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/stelianmorariu/pawz/presentation/splash/SplashActivity.kt
@@ -7,10 +7,16 @@ package com.stelianmorariu.pawz.presentation.splash
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.stelianmorariu.pawz.R
+import com.stelianmorariu.pawz.presentation.breed.list.BreedsListActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        startActivity(BreedsListActivity.newIntent(this))
     }
 }
