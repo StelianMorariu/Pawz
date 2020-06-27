a6fb34623b2f29941fce30a251684d1cc9bec8c9
Stelian Morariu <stelian.morariu@gmail.com>
2020-06-27 13:14:59 +0100

    make domain model parcelable

--- app/src/main/java/com/stelianmorariu/pawz/domain/model/DogBreed.kt
+++ app/src/main/java/com/stelianmorariu/pawz/domain/model/DogBreed.kt
@@ -4,12 +4,16 @@
 
 package com.stelianmorariu.pawz.domain.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /**
  * Our domain model represents the source of truth for the  presentation layer.
  * Data acquired from different sources will be mapped to this model.
  */
+@Parcelize
 data class DogBreed(
     val displayName: String,
     val breed: String,
     val subBreed: String = ""
-)
+) : Parcelable
