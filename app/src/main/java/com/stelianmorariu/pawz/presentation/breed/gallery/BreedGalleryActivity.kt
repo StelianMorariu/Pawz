9a7b9650e11f809a2bb1a192c741fb9f71443545
Stelian Morariu <stelian.morariu@gmail.com>
2020-06-27 16:00:46 +0100

    wire up BreedGalleryActivity

--- app/src/main/java/com/stelianmorariu/pawz/presentation/breed/gallery/BreedGalleryActivity.kt
+++ app/src/main/java/com/stelianmorariu/pawz/presentation/breed/gallery/BreedGalleryActivity.kt
@@ -6,16 +6,26 @@ package com.stelianmorariu.pawz.presentation.breed.gallery
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.Animatable2
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.stelianmorariu.pawz.R
 import com.stelianmorariu.pawz.databinding.ActivityBreedGalleryBinding
 import com.stelianmorariu.pawz.domain.dagger.utils.Injectable
 import com.stelianmorariu.pawz.domain.model.DogBreed
+import com.stelianmorariu.pawz.presentation.common.loadImage
+import com.stelianmorariu.pawz.presentation.common.widgets.GridLayoutItemSpaceDecorator
 import javax.inject.Inject
+
 
 class BreedGalleryActivity : AppCompatActivity(), Injectable {
 
@@ -28,6 +38,11 @@ class BreedGalleryActivity : AppCompatActivity(), Injectable {
     private val viewModel by lazy {
         ViewModelProvider(this, viewModelFactory).get(BreedGalleryViewModel::class.java)
     }
+
+    private var loadingAnimation: AnimatedVectorDrawable? = null
+    private var loading = false
+
+    private var breedGalleryAdaper = BreedGalleryAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,8 +59,16 @@ class BreedGalleryActivity : AppCompatActivity(), Injectable {
 
         initToolbar()
         initRecyclerView()
+
+        viewModel.viewState.observe(this, Observer { viewState ->
+            updateUiState(viewState)
+        })
     }
 
+    override fun onStart() {
+        super.onStart()
+        viewModel.loadDataIfNecessary(currentBreed)
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -57,6 +80,50 @@ class BreedGalleryActivity : AppCompatActivity(), Injectable {
         }
     }
 
+    private fun updateUiState(viewState: BreedGalleryViewState) {
+        stopAnimation()
+        when (viewState) {
+            is ErrorState -> renderErrorState(viewState)
+            is LoadingState -> renderLoadingState()
+            is DisplayGalleryState -> renderBreedImageGallery(viewState)
+        }
+    }
+
+    private fun renderBreedImageGallery(viewState: DisplayGalleryState) {
+        binding.root.transitionToState(R.id.breed_gallery_state_default)
+        breedGalleryAdaper.setItems(viewState.images)
+    }
+
+    private fun renderLoadingState() {
+        loading = true
+        binding.root.transitionToState(R.id.breed_list_state_loading)
+        loadingAnimation = binding.loadingLayout.loadingIv.drawable as AnimatedVectorDrawable
+        startAnimating()
+    }
+
+    private fun renderErrorState(viewState: ErrorState) {
+        binding.root.transitionToState(R.id.breed_list_state_error)
+
+        binding.errorLayout.imageView.loadImage(viewState.pawzError.imageId)
+        binding.errorLayout.errorMessageTv.text =
+            getString(viewState.pawzError.localizedDisplayMessageResId)
+    }
+
+    private fun startAnimating() {
+        loadingAnimation?.registerAnimationCallback(object : Animatable2.AnimationCallback() {
+            override fun onAnimationEnd(drawable: Drawable?) {
+                if (loading) {
+                    (drawable as Animatable).start()
+                }
+            }
+        })
+
+        loadingAnimation?.start()
+    }
+
+    private fun stopAnimation() {
+        loadingAnimation?.stop()
+    }
 
     private fun initToolbar() {
         setSupportActionBar(binding.toolbar)
@@ -69,11 +136,15 @@ class BreedGalleryActivity : AppCompatActivity(), Injectable {
     }
 
     private fun initRecyclerView() {
+        val spacingInPixels = resources.getDimensionPixelSize(R.dimen.grid_layout_item_spacing)
+        val columns: Int = resources.getInteger(R.integer.gallery_columns)
+
         binding.breedImageRecyclerView.apply {
-
-            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-
+            adapter = breedGalleryAdaper
+            layoutManager =
+                GridLayoutManager(context, columns, RecyclerView.VERTICAL, false)
             overScrollMode = View.OVER_SCROLL_NEVER
+            addItemDecoration(GridLayoutItemSpaceDecorator(spacingInPixels, 2))
         }
     }
 
