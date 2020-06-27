c21416c5596d15e39a96ea835faad84713125f17
Stelian Morariu <stelian.morariu@gmail.com>
2020-06-27 12:58:04 +0100

    add service method to get breed images

--- app/src/main/java/com/stelianmorariu/pawz/data/network/DogApiService.kt
+++ app/src/main/java/com/stelianmorariu/pawz/data/network/DogApiService.kt
@@ -7,6 +7,7 @@ package com.stelianmorariu.pawz.data.network
 
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface DogApiService {
@@ -14,8 +15,25 @@ interface DogApiService {
     @GET(ALL_BREADS)
     fun getAllBreads(): Single<DogBreedListDto>
 
+    @GET(BREED_GALLERY)
+    fun getBreedImageList(
+        @Path(BREED_PATH_PARAM) breed: String
+    ): Single<BreedImageListDto>
+
+    @GET(SUB_BREED_GALLERY)
+    fun getSubBreedImageList(
+        @Path(BREED_PATH_PARAM) breed: String,
+        @Path(SUBBREED_PATH_PARAM) subBreed: String
+    ): Single<BreedImageListDto>
+
     companion object {
+        const val BREED_PATH_PARAM = "breed"
+        const val SUBBREED_PATH_PARAM = "sub-breed"
         const val ALL_BREADS = "breeds/list/all"
+        const val BREED_GALLERY = "breed/{${BREED_PATH_PARAM}}/images"
+        const val SUB_BREED_GALLERY = "breed/{${BREED_PATH_PARAM}}/{${SUBBREED_PATH_PARAM}}/images"
+
+
     }
 
 }
