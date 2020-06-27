a849e6f70886d3ff0666ede67dc4c319630c497b
Stelian Morariu <stelian.morariu@gmail.com>
2020-06-27 13:14:36 +0100

    create stubbed BreedGalleryActivity

---
+++ app/src/main/java/com/stelianmorariu/pawz/presentation/breed/gallery/BreedGalleryActivity.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) Stelian Morariu 2020.
+ */
+
+package com.stelianmorariu.pawz.presentation.breed.gallery
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.stelianmorariu.pawz.databinding.ActivityBreedGalleryBinding
+import com.stelianmorariu.pawz.domain.model.DogBreed
+
+class BreedGalleryActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityBreedGalleryBinding
+    private lateinit var currentBreed: DogBreed
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        try {
+            currentBreed = intent.getParcelableExtra(BREED)!!
+        } catch (e: Exception) {
+            // todo show error message to user
+            finish()
+        }
+
+        binding = ActivityBreedGalleryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initToolbar()
+        initRecyclerView()
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    private fun initToolbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setDisplayShowHomeEnabled(false)
+            it.title = currentBreed.displayName.toUpperCase()
+        }
+
+    }
+
+    private fun initRecyclerView() {
+        binding.breedImageRecyclerView.apply {
+
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+
+            overScrollMode = View.OVER_SCROLL_NEVER
+        }
+    }
+
+    companion object {
+
+        private const val BREED = "com.stelianmorariu.pawz.BREED"
+
+        fun newIntent(context: Context, breed: DogBreed): Intent {
+            val intent = Intent(context, BreedGalleryActivity::class.java)
+            intent.putExtra(BREED, breed)
+            return intent
+        }
+    }
+}
